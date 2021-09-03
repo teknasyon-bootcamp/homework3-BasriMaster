005350819b14f674cf130f6f16e0d1ed1d4a7a52
Hasan Basri <69991414+BasriMaster@users.noreply.github.com>
2021-09-03 20:23:10 +0300

    Update form.php

--- form.php
+++ form.php
@@ -24,3 +24,99 @@
  * 
  * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
  */
+
+
+<?php
+
+/**
+ * UML diyagramında yer alan Form sınıfını oluşturmanız beklenmekte.
+ * 
+ * Sınıf içerisinde static olmayan `fields`, `action` ve `method`
+ * özellikleri (property) olması gerekiyor.
+ * 
+ * Sınıf içerisinde static olan ve Form nesnesi döndüren `createPostForm`,
+ * `createGetForm` ve `createForm` methodları bulunmalı. Bu metodlar isminde de
+ * belirtilen metodlarda Form nesneleri oluşturmalı.
+ * 
+ * Sınıf içerisinde bir "private" başlatıcı (constructor) bulunmalı. Bu başlatıcı
+ * içerisinden `action` ve `method` değerleri alınıp ilgili property'lere değerleri
+ * aktarılmalıdır.
+ * 
+ * Sınıf içerisinde static "olmayan" aşağıdaki metodlar bulunmalıdır.
+ *   - `addField` metodu `fields` property dizisine değer eklemelidir.
+ *   - `setMethod` metodu `method` propertysinin değerini değiştirmelidir.
+ *   - `render` metodu form'un ilgili alanlarını HTML çıktı olarak verip bir buton çıktıya eklemelidir.
+ * 
+ * Sonuç ekran görüntüsüne `result.png` dosyasından veya `result.html` dosyasından ulaşabilirsiniz.
+ * `app.php` çalıştırıldığında `result.html` ile aynı çıktıyı verecek şekilde geliştirme yapmalısınız.
+ * 
+ * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
+ */
+
+
+    class Form {
+
+        public function __construct(
+
+            private string $action,
+
+            private string $method,
+
+            private array &$fields,
+
+        ){
+
+            $this->action = $action;
+
+            $this->method = $method;
+
+        }
+
+
+        public function addFields($fields) : void {
+            $this->fields[] = $fields;
+        }
+
+        public function setMethod(string $method): void {
+
+            $this->method = $method; 
+
+
+        }
+        
+        public function render(){
+            
+            include './form2.php'; // HTML de düzenlediğim formu include ile render fonksiyonuna çekip gerekli parametreleri almasını sağlıyorum
+
+        }
+
+        public static function createPostForm(string $action): Form 
+        {
+            
+            $getPostForm = self::createForm($action, "POST");
+
+            return $getPostForm;
+    
+        }
+
+        public static function createGetForm(string $action): Form 
+        {
+
+            $getGetForm = self::createForm($action, "GET");
+
+            return $getGetForm;
+            
+        }  
+        
+        
+        public static function createForm(string $action, string $method): Form 
+        {
+            $Form = new Form($action, $method);
+
+            return $Form;
+        }
+
+
+
+    }
+
